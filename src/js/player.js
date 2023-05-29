6db365bf488228a241cc845f9b724f8bc69ba028
CesianaHZ <davidj.poels@gmail.com>
2023-05-29 16:27:56 +0200

    Update player.js

--- src/js/player.js
+++ src/js/player.js
@@ -4,31 +4,55 @@ import { Resources } from "./resources";
 export class Player extends ex.Actor {
     constructor() {
         super({ width: Resources.Reticle.width, height: Resources.Reticle.height });
+
+        this.colliderId = 15; // Assign a unique ID to the Player's Collider
     }
 
     onInitialize(engine) {
         super.onInitialize(engine); // Call base onInitialize
 
-        this.anchor = new ex.Vector(5, -10);
+        this.anchor = new ex.Vector(0.5, 0.5);
 
         this.graphics.use(Resources.Reticle.toSprite());
         this.w = Resources.Reticle.width;
         this.h = Resources.Reticle.height;
-        this.pos = new ex.Vector(100, 100);
-        this.scale = new ex.Vector(10, 10);
+        this.pos = new ex.Vector(200, 200);
+        this.scale = new ex.Vector(0.2, 0.2);
 
         // Enable gamepad support
         engine.input.gamepads.enabled = true;
+    }
 
-        // Add event listener for gamepad input
-        engine.on('update', () => {
-            const xAxisValue = engine.input.gamepads.at(0).getAxes(ex.Input.Axes.LeftStickX);
-            const yAxisValue = engine.input.gamepads.at(0).getAxes(ex.Input.Axes.LeftStickY);
+    onPreUpdate(engine) {
+        const xAxisValue = engine.input.gamepads.at(0).getAxes(ex.Input.Axes.LeftStickX);
+        const yAxisValue = engine.input.gamepads.at(0).getAxes(ex.Input.Axes.LeftStickY);
 
-            // Set player velocity based on gamepad input
-            const speed = 100; // Adjust the speed as needed
-            this.vel = new ex.Vector(xAxisValue * speed, yAxisValue * speed);
-        });
+        // Set player velocity based on gamepad input
+        const speed = 100; // Adjust the speed as needed
+        this.vel = new ex.Vector(xAxisValue * speed, yAxisValue * speed);
+
+        this.keyboardFunctionality(engine);
+    }
+
+
+    keyboardFunctionality(engine) {
+        let xSpeed = 0
+        let ySpeed = 0
+        let kb = engine.input.keyboard
+
+        if (kb.isHeld(ex.Input.Keys.W) || kb.isHeld(ex.Input.Keys.Up)) {
+            ySpeed = -300
+        }
+        if (kb.isHeld(ex.Input.Keys.S) || kb.isHeld(ex.Input.Keys.Down)) {
+            ySpeed = 300
+        }
+        if (kb.isHeld(ex.Input.Keys.A) || kb.isHeld(ex.Input.Keys.Left)) {
+            xSpeed = -300
+        }
+        if (kb.isHeld(ex.Input.Keys.D) || kb.isHeld(ex.Input.Keys.Right)) {
+            xSpeed = 300
+        }
+        this.vel = new ex.Vector(xspeed, yspeed)
     }
 
     onPostUpdate(engine) {
@@ -36,14 +60,19 @@ export class Player extends ex.Actor {
         this.pos = this.pos.add(this.vel.scale(engine.deltaTime));
         this.vel = ex.Vector.Zero.clone(); // Reset velocity after applying
 
-        // Make sure the player stays within the game boundaries
-        const gameWidth = engine.drawWidth;
-        const gameHeight = engine.drawHeight;
-        const halfWidth = this.width / 2;
-        const halfHeight = this.height / 2;
-        
-        this.pos.x = clamp(this.pos.x, halfWidth, gameWidth - halfWidth);
-        this.pos.y = clamp(this.pos.y, halfHeight, gameHeight - halfHeight);
+
+        this.pos.x = clamp(this.pos.x, this.width/2, engine.drawWidth - this.width/2);
+        this.pos.y = clamp(this.pos.y, this.height/3, engine.drawHeight - this.height/2);
+
+        // Output the collider ID and position if it's outside the world bounds
+        if (
+            this.pos.x < this.width/2 ||
+            this.pos.x > engine.drawWidth - this.width/2 ||
+            this.pos.y < this.height/3 ||
+            this.pos.y > engine.drawHeight - this.height/2
+        ) {
+            console.warn(`Collider with ID ${this.colliderId} is outside the world bounds.`);
+        }
     }
 }
 
